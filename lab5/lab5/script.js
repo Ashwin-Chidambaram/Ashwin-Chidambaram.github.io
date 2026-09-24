cedb0db137fe2d305d4b98e8c4b75e0c91d7d4a0
Ashwin-Chidambaram <ASHWINC@email.sc.edu>
2026-09-24 13:44:42 -0400

    More Edits/Changes To Buttons

--- lab5/lab5/script.js
+++ lab5/lab5/script.js
@@ -1,5 +1,6 @@
 let accountBalance = 100;
 const DepositAmount = 30;
+const WithdrawAmount = 30;
 
 function DepositAmount() {
     accountBalance = accountBalance + DepositAmount;
@@ -9,8 +10,33 @@ function DepositAmount() {
 
     if(accountBalance > 0)
     {
-        accountText.innerText = playerHealth;
-        statusText.innerText = "You've been hit!";
+        accountText.innerText = accountBalance;
+        statusText.innerText = "You've successfully added $10 to your bank account!";
+    }
+    else
+    {
+        accountText.innerText = 0;
+        statusText.innerText = "Bank Acount Balance $0";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "You're out of money!";
+    }
+}
+
+function WithdrawAmount() {
+    accountBalance = accountBalance - WithdrawAmount;
+
+    const accountText = document.getElementById("Account Balance");
+    const statusText = document.getElementById("deposit/withdraw-message");
+
+    if(accountBalance > 0)
+    {
+        accountText.innerText = accountBalance;
+        statusText.innerText = "You've successfully withdrawn $10 to your bank account!";
     }
     else
     {
