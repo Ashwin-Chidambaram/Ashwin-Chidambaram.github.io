776b71fcb1522efe5a50d19bdc95e6224da14476
Ashwin-Chidambaram <ASHWINC@email.sc.edu>
2026-09-25 20:46:21 -0400

    Fixed Buttons?

--- lab5/lab5/script.js
+++ lab5/lab5/script.js
@@ -8,46 +8,40 @@ function depositMoney() {
 
     const accountText = document.getElementById("account-balance");
     const statusText = document.getElementById("deposit-withdraw-message");
-    const depositButton = document.getElementById("deposit-button");
-    const withdrawButton = document.getElementById("withdraw-button");
 
     accountText.innerText = "$" + accountBalance;
-    statusText.innerText = "You've successfully deposited $30 into your bank account!";
+
+    statusText.innerText =
+        "You've successfully deposited $30 into your bank account!";
 
     statusText.style.color = "white";
     statusText.style.fontWeight = "normal";
-    document.body.style.backgroundColor = "darkcyan";
 
-    // Show withdraw button again after depositing
-    depositButton.style.display = "none";
-    withdrawButton.style.display = "inline-block";
+    document.body.style.backgroundColor = "darkcyan";
 }
 
 function withdrawMoney() {
     const accountText = document.getElementById("account-balance");
     const statusText = document.getElementById("deposit-withdraw-message");
-    const depositButton = document.getElementById("deposit-button");
-    const withdrawButton = document.getElementById("withdraw-button");
 
     if (accountBalance >= withdrawAmount) {
         accountBalance -= withdrawAmount;
 
         accountText.innerText = "$" + accountBalance;
-        statusText.innerText = "You've successfully withdrawn $30 from your bank account!";
+
+        statusText.innerText =
+            "You've successfully withdrawn $30 from your bank account!";
 
         statusText.style.color = "white";
         statusText.style.fontWeight = "normal";
+
         document.body.style.backgroundColor = "darkcyan";
-
-        // If balance reaches $0, switch to the deposit button
-        if (accountBalance === 0) {
-            withdrawButton.style.display = "none";
-            depositButton.style.display = "inline-block";
-        }
-    }
-
+    } 
+    
     else {
-        statusText.innerText = "You don't have enough money to withdraw $30!";
+        statusText.innerText =
+            "You don't have enough money to withdraw $30!";
+
         statusText.style.color = "#f9331d";
         statusText.style.fontWeight = "bold";
 
